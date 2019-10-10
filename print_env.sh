c9960bd1cd624f65a0d496bc17fb93e50e579e3a
Christian Zufferey <christian@zufferey.com>
2019-10-10 09:36:01 +0200

    wip

--- print_env.sh
+++ print_env.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #Petit script pour voir les varaibles d'env pour InfluxDB
-#zf191010.0910
+#zf191010.0935
 
 for i in $(env |grep dbflux)
 do
-    echo " env "$i
+    echo " export "$i
 done
 
