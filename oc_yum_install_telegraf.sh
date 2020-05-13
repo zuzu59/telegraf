2ef32780549b09d0cb1649d2d052dbfad61d3693
Christian Zufferey <christian@zufferey.com>
2020-05-13 14:50:51 +0000

    Commencé l'installation de Telegraf dans un container OC mais sous Centos

---
+++ oc_yum_install_telegraf.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#Petit script pour installer Telegraf dans un container Docker sur Openshift mais sous Centos et non Debian
+#zf200513.1648
+
+#source: https://computingforgeeks.com/install-and-configure-telegraf-on-rhel-centos/
+
+
+
+exit
+
+
+
+
+apt update
+apt install sudo wget
+./install.sh
+./docker_start.sh
+
+
