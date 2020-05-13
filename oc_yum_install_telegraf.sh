df9651de59af8778c700550ea47cc502c3589c0e
Christian Zufferey <christian@zufferey.com>
2020-05-13 15:10:09 +0000

    wip

--- oc_yum_install_telegraf.sh
+++ oc_yum_install_telegraf.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 #Petit script pour installer Telegraf dans un container Docker sur Openshift mais sous Centos et non Debian
-#zf200513.1654
+#zf200513.1709
 
 #source: https://computingforgeeks.com/install-and-configure-telegraf-on-rhel-centos/
 
-cat <<EOF | sudo tee /etc/yum.repos.d/influxdb.repo [influxdb] name = InfluxDB Repository - RHEL baseurl = https://repos.influxdata.com/rhel/7/x86_64/stable/ enabled = 1 gpgcheck = 1 exit gpgkey = 
-https://repos.influxdata.com/influxdb.key
+
+cat <<EOF | sudo tee /etc/yum.repos.d/influxdb.repo
+[influxdb]
+name = InfluxDB Repository - RHEL
+baseurl = https://repos.influxdata.com/rhel/7/x86_64/stable/
+enabled = 1
+gpgcheck = 1
+gpgkey = https://repos.influxdata.com/influxdb.key
 EOF
+
 
 sudo yum -y install telegraf
 
