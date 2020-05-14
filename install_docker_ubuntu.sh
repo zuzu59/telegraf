1f4a331b0760502b6c98bad2f9e2d5fc2c2dce53
Christian Zufferey <christian@zufferey.com>
2020-05-14 15:35:42 +0200

    Refactorise les nom de fichiers d'installation

--- install_docker_ubuntu.sh
+++ install_docker_ubuntu.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #Petit script pour installer Telegraf dans un container Docker sur Openshift
-#zf191010.1033
+#zf191010.1033, zf200514.1530
 
 apt update
-apt install sudo wget
+apt install -y sudo wget
 ./install.sh
 ./docker_start.sh
 
