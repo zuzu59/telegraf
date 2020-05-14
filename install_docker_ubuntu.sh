db9ef99dbe398182a9af2cba3f0f2c911edac5e3
Christian Zufferey <christian@zufferey.com>
2020-05-14 16:02:16 +0200

    wip

--- install_docker_ubuntu.sh
+++ install_docker_ubuntu.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #Petit script pour installer Telegraf dans un container Docker sur Openshift
-#zf191010.1033, zf200514.1530
+#zf191010.1033, zf200514.1559
 
 apt update
 apt install -y sudo wget
-./install.sh
+sleep 3
+./install_linux_ubuntu.sh
 ./docker_start.sh
 
 
