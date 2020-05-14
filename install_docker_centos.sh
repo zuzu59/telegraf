0630c1609085ce02476fe9372c54764afcd597de
Christian Zufferey <christian@zufferey.com>
2020-05-14 23:43:16 +0200

    Refactorise les scripts de start

--- install_docker_centos.sh
+++ install_docker_centos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Petit script pour installer Telegraf dans un container Docker sur Openshift mais sous Centos et non Debian
-#zf200513.1709
+#zf200514.2338
 
 #source: https://computingforgeeks.com/install-and-configure-telegraf-on-rhel-centos/
 
@@ -21,14 +21,5 @@ sudo yum -y install telegraf
 curl -i -XPOST "$dbflux_srv_host:$dbflux_srv_port/query?u=$dbflux_u_admin&p=$dbflux_p_admin" --data-urlencode "q=CREATE DATABASE telegraf"
 curl -i -XPOST "$dbflux_srv_host:$dbflux_srv_port/query?u=$dbflux_u_admin&p=$dbflux_p_admin" --data-urlencode "q=GRANT ALL ON telegraf TO $dbflux_u_user"
 ./patch_telegraf_config.sh
-
-exit
-
-
-
-apt update
-apt install sudo wget
-./install.sh
-./docker_start.sh
 
 
