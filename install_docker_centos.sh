dce916169b3efe39f51fd0376ae7176686311110
Christian Zufferey <christian@zufferey.com>
2020-05-15 10:24:01 +0200

    Plus besoin de créer à chaque fois la db sur InfluxDB

--- install_docker_centos.sh
+++ install_docker_centos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Petit script pour installer Telegraf dans un container Docker sur Openshift mais sous Centos et non Debian
-#zf200515.0954
+#zf200515.1023
 
 #source: https://computingforgeeks.com/install-and-configure-telegraf-on-rhel-centos/
 
@@ -17,9 +17,10 @@ EOF
 
 sudo yum -y install telegraf
 
+#A faire seulement si Influxdb n'est pas configuré !
+#curl -i -XPOST "$dbflux_srv_host:$dbflux_srv_port/query?u=$dbflux_u_admin&p=$dbflux_p_admin" --data-urlencode "q=CREATE DATABASE telegraf"
+#curl -i -XPOST "$dbflux_srv_host:$dbflux_srv_port/query?u=$dbflux_u_admin&p=$dbflux_p_admin" --data-urlencode "q=GRANT ALL ON telegraf TO $dbflux_u_user"
 
-curl -i -XPOST "$dbflux_srv_host:$dbflux_srv_port/query?u=$dbflux_u_admin&p=$dbflux_p_admin" --data-urlencode "q=CREATE DATABASE telegraf"
-curl -i -XPOST "$dbflux_srv_host:$dbflux_srv_port/query?u=$dbflux_u_admin&p=$dbflux_p_admin" --data-urlencode "q=GRANT ALL ON telegraf TO $dbflux_u_user"
 $(/usr/bin/dirname $0)/patch_telegraf_config.sh
 
 
