b9304ff6989bb13ed5d8bdc5dcb0f2fc98257a27
Christian Zufferey <christian@zufferey.com>
2020-11-10 10:52:26 +0100

    Ajouté l'installation de la sonde Telegraf dédiée pour les containers AWX sur OC

---
+++ install_docker_centos_awx.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#Petit script pour installer Telegraf dans un container Docker AWX sur Openshift sous Centos
+#zf201110.1049
+
+#source: https://computingforgeeks.com/install-and-configure-telegraf-on-rhel-centos/
+
+#BASH_XTRACEFD="5" && PS4='$LINENO: ' && set -e -v -x
+
+cat <<EOF | sudo tee /etc/yum.repos.d/influxdb.repo
+[influxdb]
+name = InfluxDB Repository - RHEL
+baseurl = https://repos.influxdata.com/rhel/7/x86_64/stable/
+enabled = 1
+gpgcheck = 1
+gpgkey = https://repos.influxdata.com/influxdb.key
+EOF
+
+
+sudo yum -y install telegraf
+
+#A faire seulement si Influxdb n'est pas configuré !
+#curl -i -XPOST "$dbflux_srv_host:$dbflux_srv_port/query?u=$dbflux_u_admin&p=$dbflux_p_admin" --data-urlencode "q=CREATE DATABASE telegraf"
+#curl -i -XPOST "$dbflux_srv_host:$dbflux_srv_port/query?u=$dbflux_u_admin&p=$dbflux_p_admin" --data-urlencode "q=GRANT ALL ON telegraf TO $dbflux_u_user"
+
+$(/usr/bin/dirname $0)/patch_telegraf_config_awx.sh
+
+
---
+++ patch_telegraf_config_awx.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Petit script pour copier et 'patcher' la config Telegraf dans un container Docker AWX sur Openshift sous Centos avec les bons sercrets qui se trouvent dans les variable d'environnements
+#zf191010.0855, zf200629.1057, zf201110.1050
+
+#BASH_XTRACEFD="5" && PS4='$LINENO: ' && set -e -v -x
+
+zfile="/etc/telegraf/telegraf.conf"
+
+sudo cp $(/usr/bin/dirname $0)/telegraf_awx.conf $zfile
+
+sudo sed -i -e "s/dbflux_srv_host/$dbflux_srv_host/g" $zfile
+sudo sed -i -e "s/dbflux_srv_port/$dbflux_srv_port/g" $zfile
+sudo sed -i -e "s/dbflux_u_user/$dbflux_u_user/g" $zfile
+sudo sed -i -e "s/dbflux_p_user/$dbflux_p_user/g" $zfile
+
+
